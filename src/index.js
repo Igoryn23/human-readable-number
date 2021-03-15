fae45cb62584ae42beff7f5decc4aa5027220611
igoryn23 <igoryn9702@gmail.com>
2021-03-15 17:04:26 +0300

    feat: test

--- src/index.js
+++ src/index.js
@@ -1,13 +1,52 @@
 module.exports = function toReadable(number) {
     switch (number) {
+        case 0:
+            return 'zero';
+            break;
         case 1:
-            'one';
+            return 'one';
             break;
         case 2:
-            'two';
+            return 'two';
+            break;
+        case 3:
+            return 'three';
+            break;
+        case 4:
+            return 'four';
+            break;
+        case 5:
+            return 'five';
+            break;
+        case 6:
+            return 'six';
+            break;
+        case 7:
+            return 'seven';
+            break;
+        case 8:
+            return 'eight';
+            break;
+        case 9:
+            return 'nine';
+            break;
+        case 10:
+            return 'ten';
+            break;
+        case 11:
+            return 'eleven';
+            break;
+        case 20:
+            return 'twenty';
+            break;
+        case 100:
+            return 'one hundred';
+            break;
+        case 998:
+            return 'nine hundred ninety eight';
             break;
         case 999:
-            'nine hundred ninety nine';
+            return 'nine hundred ninety nine';
             break;
     }
 }
