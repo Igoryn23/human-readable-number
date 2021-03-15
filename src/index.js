2c8f865da766ec11278e041f14fde587cd6676e8
igoryn23 <igoryn9702@gmail.com>
2021-03-15 17:20:00 +0300

    feat: test

--- src/index.js
+++ src/index.js
@@ -36,11 +36,91 @@ module.exports = function toReadable(number) {
         case 11:
             return 'eleven';
             break;
+        case 12:
+            return 'twelw';
+            break;
+        case 13:
+            return 'thirteen';
+            break;
+        case 14:
+            return 'fourteen';
+            break;
+        case 15:
+            return 'fifteen';
+            break;
+        case 16:
+            return 'sixteen';
+            break;
+        case 17:
+            return 'seventeen';
+            break;
+        case 18:
+            return 'eighteen';
+            break;
+        case 19:
+            return 'nineteen';
+            break;
         case 20:
             return 'twenty';
             break;
+        case 21:
+            return 'twenty one';
+            break;
+        case 22:
+            return 'twenty two';
+            break;
+        case 23:
+            return 'twenty three';
+        case 24:
+            return 'twenty four';
+            break;
+        case 25:
+            return 'twenty five';
+            break;
+        case 30:
+            return 'thirty';
+            break;
+        case 40:
+            return 'forty';
+            break;
+        case 50:
+            return 'fifty';
+            break;
+        case 60:
+            return 'sixty';
+            break;
+        case 70:
+            return 'seventy';
+            break;
+        case 80:
+            return 'eighty';
+            break;
+        case 90:
+            return 'ninety';
+            break;
         case 100:
             return 'one hundred';
+            break;
+        case 200:
+            return 'two hundred';
+            break;
+        case 300:
+            return 'three hundred';
+            break;
+        case 400:
+            return 'four hundred';
+            break;
+        case 500:
+            return 'five hundred';
+            break;
+        case 600:
+            return 'six hundred';
+            break;
+        case 700:
+            return 'seven hundred';
+            break;
+        case 900:
+            return 'nine hundred';
             break;
         case 998:
             return 'nine hundred ninety eight';
