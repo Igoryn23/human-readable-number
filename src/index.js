71fbce5241a6df756f64afa57c49504493a18bd7
igoryn23 <igoryn9702@gmail.com>
2021-03-15 16:50:58 +0300

    feat: test

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
-module.exports = function toReadable (number) {
-  
+module.exports = function toReadable(number) {
+    switch (number) {
+        case 1:
+            'one';
+            break;
+        case 2:
+            'two';
+            break;
+        case 999:
+            'nine hundred ninety nine';
+            break;
+    }
 }
