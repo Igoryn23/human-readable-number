eecfdd31f76c7baf5f6abe236a1eea2eddf7b2b5
Igoryn23 <igoryn9702@gmail.com>
2021-08-16 23:09:41 +0300

    feat: all complete

--- src/index.js
+++ src/index.js
@@ -1,88 +1,54 @@
 module.exports = function toReadable(number) {
-    const arr1 = ['zero','one','two','three','four','five','six','seven','eight','nine','ten','eleven','twelve','thirteen','fourteen','fifteen','sixteen','seventeen','eighteen','nineteen'];
-    const arr2 = ['','','twenty','thirty','forty','fifty','sixty','seventy','eighty','ninety'];
-    let temp;
-    switch(number){
-        case 20:
-            return 'twenty';
-            break;
-        case 30:
-            return 'thirty';
-            break;
-        case 30:
-            return 'thirty';
-            break;
-        case 40:
-            return 'forty';
-            break;
-        case 50:
-            return 'fifty';
-            break;
-        case 60:
-            return 'sixty';
-            break;
-        case 70:
-            return 'seventy';
-            break;
-        case 80:
-            return 'eighty';
-            break;
-        case 90:
-            return 'ninety';
-            break;
-        case 100:
-            return 'one hundred';
-            break;
-        case 200:
-            return 'two hundred';
-            break;
-        case 300:
-            return 'three hundred';
-            break;
-        case 400:
-            return 'four hundred';
-            break;
-        case 500:
-            return 'five hundred';
-            break;
-        case 600:
-            return 'six hundred';
-            break;
-        case 700:
-            return 'seven hundred';
-            break;
-        case 800:
-            return 'eight hundred';
-            break;
-        case 900:
-            return 'nine hundred';
-            break;
-    }
-    if(number < 20){
+    const arr1 = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    const arr2 = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+    const numStr = number.toString();
+    const firstSymbol = numStr[0];
+    const secondSymbol = numStr[1];
+
+    if (number < 20) {
         let word = arr1[number];
         return word;
     }
-
-    if(number > 20 && number < 100){
-let str = number + '';
-let first = str[0];
-let second = str[1];
-let word1 = arr2[first];
-let word2 = arr1[second];
-return word1 + ' ' + word2;
+    // для круглых чисел менее 100, от 20 до 90
+    if (number > 19 && number < 100 && numStr[1] == 0) {
+        let first = numStr[0];
+        let word = arr2[first];
+        return word;
     }
-
-
-if(number > 100){
-    let str = number + '';
-    
-    let firstNum = str[0];
-    let wordFirst = arr1[firstNum];
-    // далее выполняем прошлое условие 
-    let first = str[1];
-    let second = str[2];
-    let word1 = arr2[first];
-    let word2 = arr1[second];
-    return `${wordFirst} hundred ${word1} ${word2}`;
-}   
+    if (number > 20 && number < 100) {
+        let first = numStr[0];
+        let second = numStr[1];
+        let word1 = arr2[first];
+        let word2 = arr1[second];
+        return word1 + ' ' + word2;
+    }
+    if (number >= 100 && numStr[1] == 0 && numStr[2] == 0) {
+        let first = numStr[0];
+        let word = arr1[first];
+        return word + ' hundred';
+    }
+    // для числе типа от 100 до 120, от 200 до 220 ....
+    if (number > 100 && numStr[1] < 2) {
+        let first = arr1[firstSymbol];
+        // режем первый символ строки и приводим к числу
+        let tempNum = parseInt(numStr.substring(1));
+        let second = arr1[tempNum];
+        return `${first} hundred ${second}`;
+    }
+    // выше 100 и круглые 
+    if (number > 100 && numStr[2] == 0) {
+        let first = arr1[firstSymbol];
+        let second = arr2[secondSymbol];
+        return `${first} hundred ${second}`;
+    }
+    if (number > 100) {
+        let firstNum = numStr[0];
+        let wordFirst = arr1[firstNum];
+        // далее выполняем прошлое условие 
+        let first = numStr[1];
+        let second = numStr[2];
+        let word1 = arr2[first];
+        let word2 = arr1[second];
+        return `${wordFirst} hundred ${word1} ${word2}`;
+    }
 }
